98a6c25874b57b44df800084633b4a7c105da8a3
Tri Thanh Alan Inder Kumar <trithanhalan@gmail.com>
2025-08-14 06:44:03 +0000

    auto-commit for cfed1a99-864f-4fce-b596-42883e6cd85c

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,8 +1,4 @@
-<file>
-      <absolute_file_name>/app/backend/app/db/base.py</absolute_file_name>
-      <content">from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base
 
 # Base class for all SQLAlchemy models
 Base = declarative_base()
-</content>
-    </file>
